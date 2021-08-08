033120f61b411bc68ba651baf68bf175a83ca486
fabianmuema <muemafabian@gmail.com>
2021-08-08 18:23:25 +0300

    navigation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+<navigation/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/home.vue'
+import navigation from './components/navigation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    navigation,
   }
 }
 </script>
